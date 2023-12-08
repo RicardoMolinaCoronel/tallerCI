ac2f95ffc90e0ca512f230a94ed7f130ec23b833
RicardoMolinaCoronel <ridumoli@espol.edu.ec>
2023-12-07 22:26:42 -0500

    primeros tests

--- test_library_management.py
+++ test_library_management.py
@@ -1,4 +1,7 @@
-from main.py import *
+import pytest
+from datetime import datetime, timedelta
+from main import Library, main
+
 def test_successful_checkout():
     library = Library()
     selections = [{'book_index': 0, 'quantity': 2}]
