1d5a2d8c85edf07bed26df2a10f1a6f32df346d4
RicardoMolinaCoronel <ridumoli@espol.edu.ec>
2023-12-07 22:50:03 -0500

    tests

--- test_library_management.py
+++ test_library_management.py
@@ -10,3 +10,11 @@ def test_successful_checkout():
     assert library.books[0].quantity == 8
     assert len(library.checked_out_books_tmp) == 1
     assert library.checked_out_books_tmp[0]['quantity'] == 2
+
+def test_checkout_unavailable_book():
+    library = Library()
+    selections = [{'book_index': 0, 'quantity': 10}]
+    total_late_fees = library.checkout_books(selections)
+    assert total_late_fees == 0
+    assert library.books[0].quantity == 5  # Verifica que no se restaron copias del libro
+    assert len(library.checked_out_books) == 0  # Verifica que no se prestó ningún libro
